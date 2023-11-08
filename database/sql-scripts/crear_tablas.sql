efb03359e19daaeabe2517e029b7e6b80dcf535b
Matias Correa <104183897+Works-on-my-PC@users.noreply.github.com>
2023-11-08 17:41:55 -0300

    Update crear_tablas.sql: Se añadieron constraints para las primary key, campo tea calculado a partir de la tna y modificado el campo banco

--- database/sql-scripts/crear_tablas.sql
+++ database/sql-scripts/crear_tablas.sql
@@ -6,18 +6,18 @@ CREATE TABLE cotizaciones (
     id serial NOT NULL,
 	cod_moneda char(3) NOT NULL,
 	cotizacion varchar(30) NOT NULL,
-	valor_en_ars money NOT NULL,
-    fecha_de_registro timestamp with time zone,
-    PRIMARY KEY (id)
+	valor_en_ars numeric(8, 2) NOT NULL,
+    fecha_de_registro timestamp with time zone
 );
+ALTER TABLE cotizaciones ADD CONSTRAINT pk_cotizaciones PRIMARY KEY(id);
 
 -- T: Tradicional / C: Precancelable
 CREATE TYPE tipo_pf AS ENUM ('T', 'C', 'UVA');
 CREATE TABLE plazos_fijos (
 	id serial NOT NULL,
 	tipo tipo_pf NOT NULL,
-	tna numeric(4, 2) NOT NULL,
-	tea numeric(4, 2) NOT NULL,
-	banco varchar(50) NOT NULL,
-	PRIMARY KEY (id)
+	tna numeric(6, 2) NOT NULL,
+	tea numeric(7, 3) GENERATED ALWAYS AS ((POWER((1+(tna/1200)), 12)-1)*100) STORED,
+	banco varchar(100) NOT NULL
 );
+ALTER TABLE plazos_fijos ADD CONSTRAINT pk_plazos_fijos PRIMARY KEY(id);
